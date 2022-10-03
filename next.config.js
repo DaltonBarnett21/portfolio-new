6778f2587184ea9a7abc937ab055c650e7c92c3e
dalton barnett <daltonbarnett21@yahoo.com>
2022-10-03 17:44:39 -0500

    added env

--- next.config.js
+++ next.config.js
@@ -2,10 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  env: {
-    USER: "postmaster@daltoncreates.net",
-    PASSWORD: "0d8c45cebdc059fef1e19db228cb1828-4534758e-42ae6806",
-  },
 };
 
 module.exports = nextConfig;
