8e147df3b3ce2482947368179b131ab798f64089
dalton barnett <daltonbarnett21@yahoo.com>
2022-10-05 19:13:56 -0500

    added cors

--- next.config.js
+++ next.config.js
@@ -9,7 +9,10 @@ const nextConfig = {
         source: "/api/(.*)",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://www.daltoncreates.site/",
+          },
           {
             key: "Access-Control-Allow-Methods",
             value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
